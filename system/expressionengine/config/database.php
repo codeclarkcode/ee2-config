1f38ed90863daa700b55ea62ec619bbb1f1a5cd8
Jeff Clark <jeffpclark@gmail.com>
2016-10-05 13:48:27 -0400

    Initial commit

---
+++ system/expressionengine/config/database.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'database-hostname';
+$db['expressionengine']['username'] = 'database-username';
+$db['expressionengine']['password'] = 'database-password';
+$db['expressionengine']['database'] = 'database-name';
+$db['expressionengine']['dbdriver'] = 'mysqli';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = '/file_path_to_webroot/system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
